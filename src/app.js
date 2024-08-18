2c332d6d0a3868d16bbf84120873011c8ed27ca6
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-08-18 01:45:00 -0300

    p

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import "./utils/global.js"
+import "./repository/connections.js"
+import "dotenv/config.js"
+import express  from "express";
+import  cors  from "cors";
+const servidor = express ()
+servidor.use(express.json())
+servidor.use(cors());
+
+
+
+const PORTA = process.env.PORTA
+
+servidor.listen(
+    PORTA,
+    () => console.log(`API subida com sucesso na porta ${PORTA}`));
